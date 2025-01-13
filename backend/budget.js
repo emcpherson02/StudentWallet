5e6adbb65d500f1fafb3cbdda650a93457a980ba
Jack Davis <jack_davis13@outlook.com>
2025-01-13 15:42:21 +0000

    Add update_budget & get_budgets
    added api route to get and update budget in the backend

--- backend/budget.js
+++ backend/budget.js
@@ -32,5 +32,52 @@ router.post('/add_budget', async (req, res) => {
     }
 });
 
+//Get all budgets
+router.get('/get_budgets', async (req, res) => {
+    const { userId } = req.query;
+
+    if (!userId) {
+        return res.status(400).json({error: 'Missing fields'});
+    }
+
+    try {
+        const budgetSnapshot = await db.collection('users').doc(userId).collection('budgets').get();
+        const budgets = budgetSnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+
+
+        res.status(200).json({budgets});
+    } catch (error) {
+        console.error('Error Getting Budgets: ', error);
+        return res.status(500).json({error: 'Failed to get Budgets'});
+    }
+});
+
+router.put('/update_budget/:budgetId', async (req, res) => {
+    const { userId, category, amount, period, spent, startDate, endDate } = req.body;
+    const { budgetId } = req.params;
+
+    if (!userId || !budgetId) {
+        res.status(400).json({error: 'Missing fields'});
+    }
+
+    try {
+        const budgetUpdates = {
+            ...(category && {category}),
+            ...(amount && {amount}),
+            ...(period && {period}),
+            ...(spent && {spent}),
+            ...(startDate && {startDate: new Date(startDate)}),
+            ...(endDate && {endDate: new Date(endDate)}),
+        };
+        const budgetRef = db.collection('users').doc(userId).collection('budgets').doc(budgetId);
+        await budgetRef.update(budgetUpdates);
+
+        res.status(200).json({message: 'Budget updated successfully'});
+    }catch (error) {
+        console.error('Error Updating Budget: ', error);
+        return res.status(500).json({error: 'Failed to update Budget'});
+    }
+});
+
 
 module.exports = router; // Export the router
