101e555a635e736c9f676f0c84e64f9f773d583c
Jack Davis <jack_davis13@outlook.com>
2025-02-07 14:34:55 +0000

    Add BudgetHistoryModel to index

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,11 +4,13 @@ const BudgetModel = require('./budget.model');
 const PlaidModel = require('./plaid.model');
 const AuthModel = require('./auth.model');
 const { db } = require('../config/firebase.config');
+const budgetHistoryModel = require('./budgetHistory.model');
 
 module.exports = {
     userModel: new UserModel(db),
     transactionModel: new TransactionModel(db),
     budgetModel: new BudgetModel(db),
     plaidModel: new PlaidModel(db),
-    authModel: new AuthModel(db)
+    authModel: new AuthModel(db),
+    budgetHistoryModel: new budgetHistoryModel(db)
 };
