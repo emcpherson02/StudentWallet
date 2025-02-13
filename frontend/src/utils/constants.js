fde17eb8da9f3213187b1349adcd4a73e193ef45
Jack Davis <jack_davis13@outlook.com>
2025-02-13 12:47:51 +0000

    Create categoryDropdown component and category constants

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,12 @@
+export const TRANSACTION_CATEGORIES = {
+    GROCERIES: 'Groceries',
+    RENT: 'Rent',
+    UTILITIES: 'Utilities',
+    TRANSPORTATION: 'Transportation',
+    ENTERTAINMENT: 'Entertainment',
+    EDUCATION: 'Education',
+    SHOPPING: 'Shopping',
+    HEALTH: 'Health',
+    DINING: 'Dining Out',
+    OTHER: 'Other'
+};
