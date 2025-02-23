8252f7893c2b1bfe0a3a4fecdd7e2eae0cb164ee
Jack Davis <jack_davis13@outlook.com>
2025-02-23 23:45:08 +0000

    Update Toast Container Config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import PreferencesPage from './components/PreferencesPage';
 import AboutUs from './components/AboutUs';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './styles/darkMode.css';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -34,7 +34,19 @@ function App() {
               <Route path="/about-us" element={<AboutUs />} />
               {/* Add other routes here */}
           </Routes>
-          <ToastContainer position="top-center" autoClose={3000} />
+          <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              transition={Bounce}
+          />
       </div>
       </ThemeProvider>
   );
